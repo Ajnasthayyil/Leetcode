71470885ab9adb68f8494595cd05c34311918d68
AJNAS THAYYIL <105600255+Ajnasthayyil@users.noreply.github.com>
2025-10-22 15:26:11 +0530

    Time: 36 ms (5.04%), Space: 62.2 MB (5.04%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.js
+++ 2974-minimum-number-game/2974-minimum-number-game.js
@@ -6,7 +6,7 @@ var numberGame = function(nums) {
     let result=[];
     let sorted= nums.sort((a,b) => a-b);
     console.log(sorted);
-    for(i=0;i<=sorted.length/2;i++)
+    while(sorted.length>0)
     {
         let newArray=sorted.splice(0,2).reverse();
         result=result.concat(newArray);
